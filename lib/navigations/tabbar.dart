78d90d66f162b96ed8a5c675b770474c01735458
Darryl-win L. <logossoudarryl20@gmail.com>
2024-08-10 19:21:57 +0000

    navigate pages and personalising tabbar

--- lib/navigations/tabbar.dart
+++ lib/navigations/tabbar.dart
@@ -9,18 +9,37 @@ class Tabbar extends StatefulWidget {
 }
 
 class _TabbarState extends State<Tabbar> {
+  int _selectedTab = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedTab,
+        onTap: (index) {
+          setState(() {
+            this._selectedTab = index;
+          });
+        },
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search_outlined), label: 'Search'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.library_music), label: 'Library'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
         ],
       ),
-      body: HomeView(),
+      body: Stack(children: [
+        IgnorePointer(
+          ignoring: _selectedTab != 0,
+          child: Opacity(
+            opacity: _selectedTab == 0 ? 1 : 0,
+            child: HomeView(),
+          ),
+        )
+      ]),
     );
   }
 }
