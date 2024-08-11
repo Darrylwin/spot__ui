05030457af488ad21575c42a88ca958c84d8e8e1
Darryl-win L. <logossoudarryl20@gmail.com>
2024-08-11 23:21:51 +0000

    album_view

---
+++ lib/views/album_view.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+
+class AlbumView extends StatefulWidget {
+  AlbumView({super.key});
+
+  @override
+  State<AlbumView> createState() => _AlbumViewState();
+}
+
+class _AlbumViewState extends State<AlbumView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            color: Colors.pink,
+          ),
+          SafeArea(
+            child: SingleChildScrollView(
+              physics: BouncingScrollPhysics(),
+              child: Column(
+                children: [
+                  Container(
+                    clipBehavior: Clip.none,
+                    // height: 500,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Colors.black.withOpacity(0),
+                          Colors.black.withOpacity(0),
+                          Colors.black.withOpacity(1),
+                        ],
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 40),
+                      child: Column(
+                        children: [
+                          // SizedBox(height: 40),
+                          Container(
+                            decoration: BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.5),
+                                  offset: Offset(0, 20),
+                                  blurRadius: 32,
+                                  spreadRadius: 16,
+                                ),
+                              ],
+                            ),
+                            child: Image(
+                              image: AssetImage("assets/album4.jpg"),
+                              width: MediaQuery.of(context).size.width - 120,
+                              height: MediaQuery.of(context).size.width - 120,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 16,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0 * 2),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  style: Theme.of(context).textTheme.bodySmall,
+                                  'Consequat aliquip dolor consectetur sit veniam. Consectetur ipsum qui nostrud pariatur tempor minim dolor nisi elit elit in est',
+                                ),
+                                SizedBox(height: 18),
+                                Row(
+                                  children: [
+                                    Image(
+                                      image: AssetImage("assets/logo.png"),
+                                      width: 32,
+                                      height: 32,
+                                    ),
+                                    SizedBox(width: 08),
+                                    Text("Spotify"),
+                                  ],
+                                ),
+                                SizedBox(height: 16),
+                                Text(
+                                  style: TextStyle(fontSize: 15),
+                                  "1.888.132 Likes 5h 3m",
+                                ),
+                                SizedBox(height: 16),
+                                Stack(
+                                  clipBehavior: Clip.none,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Icon(Icons.favorite),
+                                        SizedBox(width: 16),
+                                        Icon(Icons.more_horiz)
+                                      ],
+                                    ),
+                                    Positioned(
+                                      right: 0,
+                                      bottom: 0,
+                                      child: Stack(
+                                        alignment: Alignment.bottomRight,
+                                        children: [
+                                          Container(
+                                            width: 64,
+                                            height: 64,
+                                            alignment: Alignment.center,
+                                            decoration: BoxDecoration(
+                                              shape: BoxShape.circle,
+                                              color: Color(0xFF14D860),
+                                            ),
+                                            child: Icon(
+                                              Icons.play_arrow_sharp,
+                                              size: 38,
+                                            ),
+                                          ),
+                                          Container(
+                                            width: 24,
+                                            height: 24,
+                                          ),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 500,
+                    color: Colors.black,
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
