a1684c04b93c30f76c6abfd0b2c960dd677f9c39
Darryl-win L. <logossoudarryl20@gmail.com>
2024-08-11 23:21:05 +0000

    album_card component

--- lib/components/album_card.dart
+++ lib/components/album_card.dart
@@ -1,23 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:spot__ui/views/album_view.dart';
 
 class AlbumCard extends StatelessWidget {
   final ImageProvider image;
   final String label;
-  const AlbumCard({
+  final onTap;
+  AlbumCard({
     super.key,
     required this.image,
     required this.label,
+    required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Image(image: image, height: 120, width: 120, fit: BoxFit.cover,),
-        const SizedBox(height: 10),
-       Text(label),
-      ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image(
+            image: image,
+            height: 120,
+            width: 120,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(height: 10),
+          Text(label),
+        ],
+      ),
     );
   }
 }
