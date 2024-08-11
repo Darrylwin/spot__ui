33455da52f87e21868c7451470dca9304a32d9b4
Darryl-win L. <logossoudarryl20@gmail.com>
2024-08-11 08:45:59 +0000

    widget album_view

---
+++ lib/components/album_card.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AlbumCard extends StatelessWidget {
+  final ImageProvider image;
+  final String label;
+  const AlbumCard({
+    super.key,
+    required this.image,
+    required this.label,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image(image: image, height: 120, width: 120,),
+        const SizedBox(height: 10),
+       Text(label),
+      ],
+    );
+  }
+}
