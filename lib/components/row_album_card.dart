5aedd64f883ea5b8428d4bac456ecbf8f7698469
Darryl-win L. <logossoudarryl20@gmail.com>
2024-08-11 12:57:08 +0000

    Component row_album_view

---
+++ lib/components/row_album_card.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class RowAlbumCard extends StatelessWidget {
+  final AssetImage image;
+  final String label;
+  RowAlbumCard({
+    super.key,
+    required this.image,
+    required this.label,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white10,
+          borderRadius: BorderRadius.circular(4),
+        ),
+        clipBehavior: Clip.antiAlias,
+        child: Row(
+          children: [
+            Image(
+              image: image,
+              height: 48,
+              width: 48,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(width: 8),
+            Text(label),
+          ],
+        ),
+      ),
+    );
+  }
+}
