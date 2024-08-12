c22e8a8ebaaf2b74395e0a9a1ab244ea9f3a41fe
Darryl-win L. <logossoudarryl20@gmail.com>
2024-08-12 20:37:13 +0000

    search section

--- lib/views/search.dart
+++ lib/views/search.dart
@@ -10,9 +10,40 @@ class SearchView extends StatefulWidget {
 class _SearchViewState extends State<SearchView> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Search Works"),
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 40,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: AssetImage("assets/album8.jpg"),
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        "Search",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  Icon(Icons.camera_alt_outlined, size: 30, weight: 40)
+                ],
+              ),
+              Center(
+                child: Text("Search Works"),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
