7c47cfe1e711aa08a89a8cc593a6c41e1a599d67
Darryl-win L. <logossoudarryl20@gmail.com>
2024-08-13 01:37:22 +0000

    widgets genres et browser

--- lib/views/search.dart
+++ lib/views/search.dart
@@ -17,13 +17,14 @@ class _SearchViewState extends State<SearchView> {
           child: Column(
             children: [
               SizedBox(
-                height: 40,
+                height: 60,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Row(
                     children: [
+                      // search section
                       CircleAvatar(
                         radius: 23,
                         backgroundImage: AssetImage("assets/profil.jpeg"),
@@ -45,12 +46,14 @@ class _SearchViewState extends State<SearchView> {
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(5),
                 ),
+                // textfield
                 child: TextField(
                   decoration: InputDecoration(
                     fillColor: Colors.white,
                     prefixIcon: Padding(
                       padding: const EdgeInsetsDirectional.all(6.0),
-                      child: Icon(Icons.search_rounded, size: 35, color: Color.fromARGB(255, 21, 19, 19)),
+                      child: Icon(Icons.search_rounded,
+                          size: 35, color: Color.fromARGB(255, 21, 19, 19)),
                     ),
                     hintText: 'What should you listen to ?',
                     hintStyle: TextStyle(
@@ -63,6 +66,77 @@ class _SearchViewState extends State<SearchView> {
                       borderRadius: BorderRadius.circular(7),
                     ),
                   ),
+                ),
+              ),
+              SizedBox(height: 30),
+              SingleChildScrollView(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Explore your genres",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: 225,
+                width: 125,
+                child: Image.asset(
+                  "assets/album6.jpg",
+                  fit: BoxFit.cover,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              SizedBox(height: 18),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    "Browse all",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              SingleChildScrollView(
+                child: Row(
+                  children: [
+                    Container(
+                      height: 140,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        color: Colors.pink,
+                        borderRadius: BorderRadius.circular(04),
+                      ),
+                      child: Stack(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 18),
+                            child: Text(
+                              "Music",
+                              style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          // Positioned(),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
