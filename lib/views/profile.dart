f986cda817b7992866f686afb7c0f8039cffff66
Darryl-win L. <logossoudarryl20@gmail.com>
2024-09-01 13:28:37 +0000

    profile page

--- lib/views/profile.dart
+++ lib/views/profile.dart
@@ -1,29 +1,249 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors_in_immutables
+
 import 'package:flutter/material.dart';
 
-import '../components/album_rectangle.dart';
+import '../widgets/album_card.dart';
+import '../widgets/row_album_card.dart';
 
 class ProfileView extends StatefulWidget {
-  const ProfileView({super.key});
+  ProfileView({super.key});
 
   @override
   State<ProfileView> createState() => _ProfileViewState();
 }
 
 class _ProfileViewState extends State<ProfileView> {
+  ScrollController? scrollController;
+  double imageSize = 0;
+  double initialSize = 240;
+  double containerHeight = 500;
+  double containerInitialHeight = 500;
+  double imageOpacity = 1;
+  bool showTopBar = false;
+
+  @override
+  void initState() {
+    imageSize = initialSize;
+    scrollController = ScrollController()
+      ..addListener(() {
+        imageSize = initialSize - scrollController!.offset;
+        if (imageSize < 0) {
+          imageSize = 0;
+        }
+        containerHeight = containerInitialHeight - scrollController!.offset;
+        if (containerHeight < 0) {
+          containerHeight = 0;
+        }
+        imageOpacity = imageSize / initialSize;
+        if (scrollController!.offset > 224) {
+          showTopBar = true;
+        } else {
+          showTopBar = false;
+        }
+        setState(() {});
+      });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          height: 200,
-        ),
-        Center(
-          child: AlbumRectangle(
-            image: AssetImage("assets/album1.jpg"),
+    final cardSize = MediaQuery.of(context).size.width / 2 - 32;
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: containerHeight,
+            width: MediaQuery.of(context).size.width,
+            alignment: Alignment.center,
             color: Colors.pink,
+            child: Image(
+              image: AssetImage("assets/album5.jpg"),
+              // width: MediaQuery.of(context).size.width,
+              // height: MediaQuery.of(context).size.width,
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-      ],
+          SafeArea(
+            child: SingleChildScrollView(
+              controller: scrollController,
+              physics: BouncingScrollPhysics(),
+              child: Column(
+                children: [
+                  Container(
+                    clipBehavior: Clip.none,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Colors.black.withOpacity(0),
+                          Colors.black.withOpacity(0),
+                          Colors.black.withOpacity(1),
+                        ],
+                      ),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 40),
+                      child: Column(
+                        children: [
+                          SizedBox(height: initialSize + 32),
+                          Padding(
+                            padding: EdgeInsets.all(8.0 * 2),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  style: Theme.of(context).textTheme.bodySmall,
+                                  'Consequat aliquip dolor consectetur sit veniam. Consectetur ipsum qui nostrud pariatur tempor minim dolor nisi elit elit in est',
+                                ),
+                                SizedBox(height: 18),
+                                Row(
+                                  children: [
+                                    Image(
+                                      image: AssetImage("assets/logo.png"),
+                                      width: 32,
+                                      height: 32,
+                                    ),
+                                    SizedBox(width: 08),
+                                    Text("Spotify"),
+                                  ],
+                                ),
+                                SizedBox(height: 16),
+                                Text(
+                                  style: TextStyle(fontSize: 15),
+                                  "1.888.132 Likes 5h 3m",
+                                ),
+                                SizedBox(height: 16),
+                                Stack(
+                                  clipBehavior: Clip.none,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Icon(Icons.favorite),
+                                        SizedBox(width: 16),
+                                        Icon(Icons.more_horiz)
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(16),
+                    color: Colors.black,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                            "Laborum commodo irure nostrud Lorem ipsum. Aute deserunt irure excepteur duis quis est labore cillum aliqua do esse tempor exercitation aliquip. Nulla qui esse aliqua non cupidatat. Cupidatat dolor ullamco ."),
+                        SizedBox(height: 32),
+                        Text(
+                          "You might also like",
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 16),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              AlbumCard(
+                                size: cardSize,
+                                label: "Get Turnt",
+                                image: AssetImage("assets/album3.jpg"),
+                              ),
+                              AlbumCard(
+                                size: cardSize,
+                                label: "Get Turnt",
+                                image: AssetImage("assets/album5.jpg"),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 16),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              AlbumCard(
+                                size: cardSize,
+                                label: "Get Turnt",
+                                image: AssetImage("assets/album6.jpg"),
+                              ),
+                              AlbumCard(
+                                size: cardSize,
+                                label: "Get Turnt",
+                                image: AssetImage("assets/album9.jpg"),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 16),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              AlbumCard(
+                                size: cardSize,
+                                label: "Get Turnt",
+                                image: AssetImage("assets/album10.jpg"),
+                              ),
+                              AlbumCard(
+                                size: cardSize,
+                                label: "Get Turnt",
+                                image: AssetImage("assets/album4.jpg"),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            child: Container(
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 250),
+                color: showTopBar
+                    ? Color(0xFFC61855).withOpacity(1)
+                    : Color(0xFFC61855).withOpacity(0),
+                padding: EdgeInsets.symmetric(
+                  horizontal: 18,
+                  vertical: 10,
+                ),
+                child: SafeArea(
+                  child: Container(
+                    height: 40,
+                    width: MediaQuery.of(context).size.width,
+                    child: Stack(
+                      clipBehavior: Clip.none,
+                      alignment: Alignment.center,
+                      children: [
+                        AnimatedOpacity(
+                          duration: Duration(milliseconds: 250),
+                          opacity: showTopBar ? 1 : 0,
+                          child: Text(
+                            "Ophelia",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
