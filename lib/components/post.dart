d672f11c7914068fd617a84fc142643b717ceb19
Darrylwin <logossoudarryl20@gmail.com>
2024-09-21 00:27:50 +0000

    post view

--- lib/components/post.dart
+++ lib/components/post.dart
@@ -3,23 +3,62 @@
 import 'package:flutter/material.dart';
 
 class Post extends StatelessWidget {
-  final Color color;
+  final List<String> imagePaths = [
+    "assets/abm26.jpg",
+    "assets/abm25.jpg",
+    "assets/abm22.jpg",
+    "assets/abm23.jpg",
+    "assets/abm21.jpg",
+    "assets/abm17.jpg",
+    "assets/abm16.jpg",
+    "assets/abm18.jpg",
+    "assets/album1.jpg",
+    "assets/album4.jpg",
+    "assets/album7.jpg",
+    "assets/album8.jpg",
+    "assets/album9.jpg",
+    "assets/album10.jpg",
+    "assets/album11.jpg",
+    "assets/album13.jpg",
+  ];
+
+  final List<Color> colors = [
+    const Color.fromARGB(255, 67, 8, 78),
+    const Color.fromARGB(255, 9, 62, 11),
+    const Color.fromARGB(255, 143, 38, 6),
+    const Color.fromARGB(255, 8, 56, 62),
+    const Color.fromARGB(255, 156, 29, 71),
+    const Color.fromARGB(255, 19, 56, 47),
+    const Color.fromARGB(255, 107, 85, 19),
+    const Color.fromARGB(255, 63, 7, 7),
+    const Color.fromARGB(255, 112, 123, 11),
+    const Color.fromARGB(255, 11, 102, 81),
+    const Color.fromARGB(255, 4, 26, 150),
+    const Color.fromARGB(255, 85, 65, 65),
+    const Color.fromARGB(255, 4, 75, 86),
+    const Color.fromARGB(255, 37, 3, 53),
+    const Color.fromARGB(255, 8, 19, 82),
+    const Color.fromARGB(255, 51, 31, 4),
+  ];
 
   Post({
     super.key,
-    required this.color,
   });
 
   @override
   Widget build(BuildContext context) {
+    imagePaths.shuffle();
+    colors.shuffle();
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.only(bottom: 15),
         child: ClipRRect(
           borderRadius: BorderRadius.circular(23),
           child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
             decoration: BoxDecoration(
-              color: color,
+              color: colors.first,
               image: DecorationImage(
                 image: AssetImage("assets/waves.png"),
                 fit: BoxFit.fitWidth,
@@ -53,69 +92,89 @@ class Post extends StatelessWidget {
                     width: MediaQuery.of(context).size.width / 2,
                     height: MediaQuery.of(context).size.width / 2,
                     child: Image.asset(
-                      "assets/abm14.jpg",
-                      // width: MediaQuery.of(context).size.width / 2,
-                      // height: MediaQuery.of(context).size.width / 2,
+                      imagePaths.last,
                     ),
                   ),
                 ),
                 Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Row(
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(8),
-                          child: Image.asset(
-                            "assets/abm18.jpg",
-                            height: 55,
-                            width: 55,
-                          ),
-                        ),
-                        SizedBox(width: 18),
-                        Column(
+                        Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            const Text(
-                              "DODO",
-                              style: TextStyle(
-                                fontSize: 21,
-                                fontWeight: FontWeight.bold,
-                              ),
+                            Text(
+                              "#Pop music",
                             ),
                             Text(
-                              'TAYC',
-                              style: TextStyle(
-                                color: const Color.fromARGB(255, 165, 163, 163),
-                                fontSize: 13.7,
-                                // fontWeight: FontWeight.bold,
+                              "#French music",
+                            ),
+                            Text(
+                              "#Afro beat",
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 15),
+                        Row(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(8),
+                              child: Image.asset(
+                                imagePaths.last,
+                                height: 55,
+                                width: 55,
                               ),
+                            ),
+                            SizedBox(width: 18),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  "DODO",
+                                  style: TextStyle(
+                                    fontSize: 21,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  'TAYC',
+                                  style: TextStyle(
+                                    color: const Color.fromARGB(
+                                        255, 165, 163, 163),
+                                    fontSize: 13.7,
+                                    // fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
                             ),
                           ],
                         ),
                       ],
                     ),
-                    Icon(Icons.add_circle_outline, size: 30),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Badge(),
+                        Icon(Icons.volume_up, size: 30),
+                        const SizedBox(height: 25),
+                        Icon(Icons.share, size: 30),
+                        const SizedBox(height: 25),
+                        Icon(Icons.more_horiz, size: 30),
+                        const SizedBox(height: 25),
+                        Icon(Icons.add_circle_outline, size: 35),
+                      ],
+                    ),
                   ],
                 ),
-                // Stack(
-                //   children: [
-                //     Positioned(
-                //       top: 10,
-                //       left: 20,
-                //       child: Container(
-                //         width: 100,
-                //         height: 50,
-                //         color: Colors.blue,
-                //         child: Column(
-                //             // ... vos éléments de colonne
-                //             ),
-                //       ),
-                //     ),
-                //     // ... autres éléments
-                //   ],
-                // )
+                const SizedBox(height: 15.5),
+                Divider(
+                  color: Colors.white,
+                  thickness: 2,
+                ),
               ],
             ),
           ),
